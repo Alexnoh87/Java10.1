1ea842e020105119958a90dc51e8f58b29d627a2
Alex Nohrin <beterok@inbox.ru>
2023-06-07 19:10:36 +0500

    Revision Lombok

--- src/main/java/ru/netology/oop/Radio.java
+++ src/main/java/ru/netology/oop/Radio.java
@@ -10,11 +10,78 @@ import lombok.NoArgsConstructor;
 
 public class Radio {
 
-    private int maxCurrentStationNumber = 10;
+    private int maxCurrentStationNumber = 9;
     private int minCurrentStationNumber;
     private int currentStationNumber = minCurrentStationNumber;
     private int maxSoundVolume = 100;
     private int minSoundVolume;
     private int soundVolume = minSoundVolume;
+
+    public Radio(int sizeStation) {
+        maxCurrentStationNumber = minCurrentStationNumber + sizeStation - 1;
+        maxSoundVolume = minSoundVolume + sizeStation;
+    }
+
+    public void setCurrentStationNumber(int newCurrentStationNumber) {
+        if (newCurrentStationNumber > maxCurrentStationNumber) {
+            return;
+        }
+        if (newCurrentStationNumber < minCurrentStationNumber) {
+            newCurrentStationNumber = maxCurrentStationNumber;
+        }
+        currentStationNumber = newCurrentStationNumber;
+    }
+
+    public void setSoundVolume(int newSoundVolume) {
+        if (newSoundVolume > maxSoundVolume) {
+            newSoundVolume = maxSoundVolume;
+        }
+        if (newSoundVolume < minSoundVolume) {
+            newSoundVolume = minSoundVolume;
+        }
+        soundVolume = newSoundVolume;
+    }
+
+    public void maxCurrentStationNumber() {
+        currentStationNumber = maxCurrentStationNumber;
+    }
+
+    public void maxSoundVolume() {
+        soundVolume = maxSoundVolume;
+    }
+
+    public void nextStationNumber() {
+        if (currentStationNumber >= maxCurrentStationNumber) {
+            currentStationNumber = minCurrentStationNumber;
+            return;
+        }
+        currentStationNumber = currentStationNumber + 1;
+    }
+
+    public void prevStationNumber() {
+        if (currentStationNumber <= minCurrentStationNumber) {
+            currentStationNumber = maxCurrentStationNumber;
+            return;
+        }
+        currentStationNumber = currentStationNumber - 1;
+    }
+
+    public void moreSound() {
+        if (soundVolume >= maxSoundVolume) {
+            soundVolume = soundVolume;
+        }
+        if (soundVolume < maxSoundVolume) {
+            soundVolume = soundVolume + 1;
+        }
+    }
+
+    public void lessSound() {
+        if (soundVolume <= minSoundVolume) {
+            soundVolume = soundVolume;
+        }
+        if (soundVolume > minSoundVolume) {
+            soundVolume = soundVolume - 1;
+        }
+    }
 
 }
--- src/test/java/ru/netology/oop/RadioTest.java
+++ src/test/java/ru/netology/oop/RadioTest.java
@@ -7,13 +7,94 @@ public class RadioTest {
 
     @Test
     public void shouldSetStationNumber() {
-        Radio rad = new Radio(11, 0, 0, 100, 0, 0);
+        Radio rad = new Radio(11);
 
         rad.setCurrentStationNumber(5);
 
         Assertions.assertEquals(0, rad.getMinCurrentStationNumber());
-        Assertions.assertEquals(11, rad.getMaxCurrentStationNumber());
+        Assertions.assertEquals(10, rad.getMaxCurrentStationNumber());
         Assertions.assertEquals(5, rad.getCurrentStationNumber());
+    }
+
+    @Test
+    public void shouldNotSetStationNumberAboveMax() {
+        Radio rad = new Radio(12);
+
+        rad.setCurrentStationNumber(12);
+
+        Assertions.assertEquals(0, rad.getMinCurrentStationNumber());
+        Assertions.assertEquals(11, rad.getMaxCurrentStationNumber());
+        Assertions.assertEquals(0, rad.getCurrentStationNumber());
+    }
+
+    @Test
+    public void shouldNotSetStationNumberBelowMin() {
+        Radio rad = new Radio();
+
+        rad.setCurrentStationNumber(-1);
+
+        Assertions.assertEquals(0, rad.getMinCurrentStationNumber());
+        Assertions.assertEquals(9, rad.getMaxCurrentStationNumber());
+        Assertions.assertEquals(9, rad.getCurrentStationNumber());
+    }
+
+    @Test
+    public void shouldSetMaxStationNumber() {
+        Radio rad = new Radio(15);
+
+        rad.maxCurrentStationNumber();
+
+        Assertions.assertEquals(0, rad.getMinCurrentStationNumber());
+        Assertions.assertEquals(14, rad.getMaxCurrentStationNumber());
+        Assertions.assertEquals(14, rad.getCurrentStationNumber());
+    }
+
+    @Test
+    public void shouldNextStationNumber() {
+        Radio rad = new Radio(10);
+
+        rad.setCurrentStationNumber(5);
+        rad.nextStationNumber();
+
+        Assertions.assertEquals(0, rad.getMinCurrentStationNumber());
+        Assertions.assertEquals(9, rad.getMaxCurrentStationNumber());
+        Assertions.assertEquals(6, rad.getCurrentStationNumber());
+    }
+
+    @Test
+    public void shouldNextMoreMaxStationNumber() {
+        Radio rad = new Radio(10);
+
+        rad.setCurrentStationNumber(9);
+        rad.nextStationNumber();
+
+        Assertions.assertEquals(0, rad.getMinCurrentStationNumber());
+        Assertions.assertEquals(9, rad.getMaxCurrentStationNumber());
+        Assertions.assertEquals(0, rad.getCurrentStationNumber());
+    }
+
+    @Test
+    public void shouldPrevStationNumber() {
+        Radio rad = new Radio(10);
+
+        rad.setCurrentStationNumber(7);
+        rad.prevStationNumber();
+
+        Assertions.assertEquals(0, rad.getMinCurrentStationNumber());
+        Assertions.assertEquals(9, rad.getMaxCurrentStationNumber());
+        Assertions.assertEquals(6, rad.getCurrentStationNumber());
+    }
+
+    @Test
+    public void shouldPrevLessMinStationNumber() {
+        Radio rad = new Radio();
+
+        rad.setCurrentStationNumber(0);
+        rad.prevStationNumber();
+
+        Assertions.assertEquals(0, rad.getMinCurrentStationNumber());
+        Assertions.assertEquals(9, rad.getMaxCurrentStationNumber());
+        Assertions.assertEquals(9, rad.getCurrentStationNumber());
     }
 
     @Test
@@ -28,25 +109,83 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldSetMaxCurrentStation() {
+    public void shouldNotSetSoundVolumeAboveMax() {
         Radio rad = new Radio();
 
-        rad.setMaxCurrentStationNumber(8);
+        rad.setSoundVolume(101);
 
-        Assertions.assertEquals(0, rad.getMinCurrentStationNumber());
-        Assertions.assertEquals(8, rad.getMaxCurrentStationNumber());
-        Assertions.assertEquals(0, rad.getCurrentStationNumber());
+        Assertions.assertEquals(0, rad.getMinSoundVolume());
+        Assertions.assertEquals(100, rad.getMaxSoundVolume());
+        Assertions.assertEquals(100, rad.getSoundVolume());
     }
 
     @Test
-    public void shouldSetMaxSoundVolume() {
+    public void shouldNotSetSoundVolumeBelowMin() {
         Radio rad = new Radio();
 
-        rad.setMaxSoundVolume(100);
+        rad.setSoundVolume(-1);
 
         Assertions.assertEquals(0, rad.getMinSoundVolume());
         Assertions.assertEquals(100, rad.getMaxSoundVolume());
         Assertions.assertEquals(0, rad.getSoundVolume());
     }
 
+    @Test
+    public void shouldSetMaxSoundVolume() {
+        Radio rad = new Radio();
+
+        rad.maxSoundVolume();
+
+        Assertions.assertEquals(0, rad.getMinSoundVolume());
+        Assertions.assertEquals(100, rad.getMaxSoundVolume());
+        Assertions.assertEquals(100, rad.getSoundVolume());
+    }
+
+    @Test
+    public void shouldMoreSoundVolume() {
+        Radio rad = new Radio();
+
+        rad.setSoundVolume(52);
+        rad.moreSound();
+
+        Assertions.assertEquals(0, rad.getMinSoundVolume());
+        Assertions.assertEquals(100, rad.getMaxSoundVolume());
+        Assertions.assertEquals(53, rad.getSoundVolume());
+    }
+
+    @Test
+    public void shouldMoreMaxSoundVolume() {
+        Radio rad = new Radio();
+
+        rad.setSoundVolume(101);
+        rad.moreSound();
+
+        Assertions.assertEquals(0, rad.getMinSoundVolume());
+        Assertions.assertEquals(100, rad.getMaxSoundVolume());
+        Assertions.assertEquals(100, rad.getSoundVolume());
+    }
+
+    @Test
+    public void shouldLessSoundVolume() {
+        Radio rad = new Radio();
+
+        rad.setSoundVolume(48);
+        rad.lessSound();
+
+        Assertions.assertEquals(0, rad.getMinSoundVolume());
+        Assertions.assertEquals(100, rad.getMaxSoundVolume());
+        Assertions.assertEquals(47, rad.getSoundVolume());
+    }
+
+    @Test
+    public void shouldLessMinSoundVolume() {
+        Radio rad = new Radio();
+
+        rad.setSoundVolume(-1);
+        rad.lessSound();
+
+        Assertions.assertEquals(0, rad.getMinSoundVolume());
+        Assertions.assertEquals(100, rad.getMaxSoundVolume());
+        Assertions.assertEquals(0, rad.getSoundVolume());
+    }
 }
